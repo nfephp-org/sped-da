d0097a0c5082be7349e4ad26ad0aa7edddafdfe5
Gustavo Debiasi <gustavo-debiasi@hotmail.com>
2018-06-20 15:21:40 -0300

    Update DacteOSV3.php
    Ajustado a busca do cteProc que deveria ser cteOSProc

--- src/CTe/DacteOSV3.php
+++ src/CTe/DacteOSV3.php
@@ -152,7 +152,7 @@ class DacteOSV3 extends Common
         if (!empty($this->xml)) {
             $this->dom = new Dom();
             $this->dom->loadXML($this->xml);
-            $this->cteProc = $this->dom->getElementsByTagName("cteProc")->item(0);
+            $this->cteProc = $this->dom->getElementsByTagName("cteOSProc")->item(0);
             $this->infCte = $this->dom->getElementsByTagName("infCte")->item(0);
             $this->ide = $this->dom->getElementsByTagName("ide")->item(0);
             $this->emit = $this->dom->getElementsByTagName("emit")->item(0);
