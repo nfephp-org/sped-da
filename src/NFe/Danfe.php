1008e1df2bbe467b070147ae294957f17cbf72e9
Guilherme Calabria Filho <guiga86@gmail.com>
2018-09-20 14:52:49 -0300

    Tinha um IF duplicado

--- src/NFe/Danfe.php
+++ src/NFe/Danfe.php
@@ -752,14 +752,9 @@ class Danfe extends Common
             if (count($formaPag)=='1' && (isset($formaPag[90]) || isset($formaPag[99]))) {
                 $y = $y;
             } else {
-                //Se somente tiver a forma de pagamento sem pagamento ou outros não imprimir nada
-                if (count($formaPag)=='1' && (isset($formaPag[90]) || isset($formaPag[99]))) {
-                    $y = $y;
-                } else {
-                    //caso tenha mais de uma forma de pagamento ou seja diferente de boleto exibe a
-                    //forma de pagamento e o valor
-                    $y = $this->pagamentoDANFE($x, $y+1);
-                }
+                //caso tenha mais de uma forma de pagamento ou seja diferente de boleto exibe a
+                //forma de pagamento e o valor
+                $y = $this->pagamentoDANFE($x, $y+1);
             }
         }
         //coloca os dados dos impostos e totais da NFe
