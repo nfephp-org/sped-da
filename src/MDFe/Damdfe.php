677356e851ccc5e54e2385104d53c37b14adbfdd
Mateus Perego <mateusperego@yahoo.com.br>
2018-11-30 17:31:23 -0200

    Alteracao unidade

--- src/MDFe/Damdfe.php
+++ src/MDFe/Damdfe.php
@@ -172,7 +172,7 @@ class Damdfe extends Common
             $this->qCT = $this->dom->getElementsByTagName("qCT")->item(0)->nodeValue;
         }
         $this->qCarga = $this->dom->getElementsByTagName("qCarga")->item(0)->nodeValue;
-		$this->cUnid = $this->dom->getElementsByTagName("cUnid")->item(0)->nodeValue;
+        $this->cUnid = $this->dom->getElementsByTagName("cUnid")->item(0)->nodeValue;
         $this->infModal = $this->dom->getElementsByTagName("infModal")->item(0);
         $this->rodo = $this->dom->getElementsByTagName("rodo")->item(0);
         $this->ciot = "";
@@ -693,9 +693,9 @@ class Damdfe extends Common
         $this->pTextBox($x1, $y+4, $x2, 10, $texto, $aFont, 'T', 'C', 0, '', false);
         $x1 += $x2;
         $this->pTextBox($x1, $y, $x2, 12);
-		if ($this->cUnid == 01) {
+        if ($this->cUnid == 01) {
             $texto = 'Peso Total (Kg)';
-        }else{
+        } else {
             $texto = 'Peso Total (Ton)';
         }
         $aFont = array('font'=>$this->fontePadrao, 'size'=>8, 'style'=>'');
