10ec62dddfcdfb824336fbcc86436751071d8d5f
Pablo <pabloalessandrop@gmail.com>
2019-11-27 09:40:54 -0300

    Alteração da chamada da pTextBox na linha 876
    Bom dia,
    
    Por favor efetuar a alteração na linha 876, pois a chamada esta errada
    
    Alteração da linha 876
    
    $heigthText = $this->pTextBox($x, $y, $w, $hLinha, $texto, $aFontTit, 'C', 'C', 0, '', false);
    
    
    Para:
    
    $heigthText = $this->pdf->textBox($x, $y, $w, $hLinha, $texto, $aFontTit, 'C', 'C', 0, '', false);

--- src/NFe/Danfce.php
+++ src/NFe/Danfce.php
@@ -873,7 +873,7 @@ class Danfce extends Common
         if ($this->nfeProc->getElementsByTagName("xMsg")) {
             $texto = $texto . ' ' . $this->nfeProc->getElementsByTagName("xMsg")->item(0)->nodeValue;
         }
-        $heigthText = $this->pTextBox($x, $y, $w, $hLinha, $texto, $aFontTit, 'C', 'C', 0, '', false);
+        $heigthText = $this->pdf->textBox($x, $y, $w, $hLinha, $texto, $aFontTit, 'C', 'C', 0, '', false);
         // seta o textbox do texto adicional
         $this->pdf->textBox($x, $y+3, $w-2, $hLinha-3, $this->textoAdic, $aFontTex, 'T', 'L', 0, '', false);
     }
