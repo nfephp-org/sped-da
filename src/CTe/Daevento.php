eb40d9369994793c99a1fb97afdfa6832bf93195
Ramon Loureiro <braga_ramon@hotmail.com>
2019-12-10 18:52:16 -0300

    Parametros da função devem estar na mesma linha
    The closing parenthesis and the opening brace of a multi-line function declaration must be on the same line

--- src/CTe/Daevento.php
+++ src/CTe/Daevento.php
@@ -170,12 +170,7 @@ class Daevento extends Common
      * @param string $papel (Opcional) Estabelece o tamanho do papel (ex. A4)
      * @return string O ID do evento extraido do arquivo XML
      */
-    public function monta(
-        $logo = '',
-        $orientacao = '',
-        $papel = 'A4',
-        $logoAlign = 'C'
-    )
+    public function monta($logo = '', $orientacao = '', $papel = 'A4', $logoAlign = 'C')
     {
         if ($orientacao == '') {
             $orientacao = 'P';
@@ -248,11 +243,7 @@ class Daevento extends Common
      * @param integer $pag
      * @return integer
      */
-    private function header(
-        $x,
-        $y,
-        $pag
-    )
+    private function header($x, $y, $pag)
     {
         $oldX = $x;
         $oldY = $y;
