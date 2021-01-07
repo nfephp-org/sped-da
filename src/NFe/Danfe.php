900a59c699d3a816e8e968bccaeb45ee965f9e9e
Rodrigo Costa <rodrigocosta@outlook.it>
2021-01-07 11:54:15 -0400

    modificado o tamanho da fonte dos dados adicionais

--- src/NFe/Danfe.php
+++ src/NFe/Danfe.php
@@ -284,7 +284,7 @@ class Danfe extends DaCommon
         //informações adicionais
         $fontProduto = [
             'font' => $this->fontePadrao,
-            'size' => 8,
+            'size' => 7,
             'style' => ''
         ];
         $k = $this->pdf->k;
