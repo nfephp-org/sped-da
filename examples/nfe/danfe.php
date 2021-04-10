e69d1271779a03d126b3f4fbebc8b2cc1cf412b2
Manito <eumanito@gmail.com>
2021-04-10 11:30:54 -0300

    Instrução de uso do método
    Instrução de uso do método setOcultarUnidadeTributavel

--- examples/nfe/danfe.php
+++ examples/nfe/danfe.php
@@ -18,6 +18,8 @@ try {
     $danfe->epec('891180004131899', '14/08/2018 11:24:45'); //marca como autorizada por EPEC
     // Caso queira mudar a configuracao padrao de impressao
     /*  $this->printParameters( $orientacao = '', $papel = 'A4', $margSup = 2, $margEsq = 2 ); */
+    // Caso queira sempre ocultar a unidade tributável
+    /*  $this->setOcultarUnidadeTributavel(true); */
     //Informe o numero DPEC
     /*  $danfe->depecNumber('123456789'); */
     //Configura a posicao da logo
