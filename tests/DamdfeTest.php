cb069fdd6a422d5c214f3e227f210cc30d983c2b
Gustavo Lidani <lidanig0@gmail.com>
2022-02-16 15:28:30 -0300

    setup phpunit tests; debug sped common

--- tests/DamdfeTest.php
+++ tests/DamdfeTest.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace NFePHP\DA\Tests;
 
 /**
@@ -7,30 +8,37 @@ namespace NFePHP\DA\Tests;
  * Date: 20/12/2017
  * Time: 17:54
  */
-use NFePHP\DA\MDFe\Damdfe;
-use PHPUnit\Framework\Assert;
 
-class DamdfeTest extends \PHPUnit_Framework_TestCase
+use NFePHP\DA\MDFe\Damdfe;
+use PHPUnit\Framework\TestCase;
+
+class DamdfeTest extends TestCase
 {
     /**
      * @test
      */
 
-    public function imprimirDamdfe() {
-        return true;
-//        $pathBase = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'fixtures' . DIRECTORY_SEPARATOR;
-//        $pathXml =   $pathBase . 'xml' . DIRECTORY_SEPARATOR . 'mdfe_modelo_nao_valido.xml';
-//        $pathPdf =   $pathBase . 'pdf' . DIRECTORY_SEPARATOR;
-//        $pdfName = 'mdfe_modelo_nao_valido.pdf';
-//
-//        if (file_exists($pathPdf . $pdfName)) {
-//            unlink($pathPdf . $pdfName);
-//        }
-//
-//        $xmlFile = file_get_contents($pathXml);
-//        $damdfe = new Damdfe($xmlFile, 'P', 'A4', '','F', $pathPdf);
-//        $damdfe->printMDFe($pdfName, 'F');
-//        Assert::assertFileExists($pathPdf . $pdfName);
-    }
+    public function imprimirDamdfe()
+    {
+        // return true;
+        $pathBase = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'fixtures' . DIRECTORY_SEPARATOR;
+        $pathXml =   $pathBase . 'xml' . DIRECTORY_SEPARATOR . 'mdfe_modelo_nao_valido.xml';
+        $pathPdf =   $pathBase . 'pdf' . DIRECTORY_SEPARATOR;
+        $pdfName = 'mdfe_modelo_nao_valido.pdf';
 
+        if (file_exists($pathPdf . $pdfName)) {
+            unlink($pathPdf . $pdfName);
+        }
+
+        $xmlFile = file_get_contents($pathXml);
+
+        $damdfe = new Damdfe($xmlFile, 'P', 'A4', '', 'F', $pathPdf);
+        $pdfContent = $damdfe->render();
+
+        if ($pdfContent) {
+            file_put_contents($pathPdf . $pdfName, $pdfContent);
+        }
+
+        $this->assertFileExists($pathPdf . $pdfName);
+    }
 }
