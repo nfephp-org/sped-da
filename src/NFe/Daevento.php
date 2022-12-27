ce30b7a1ce9273a0db339c8b00860ac45c1e9ffc
robmachado <linux.rlm@gmail.com>
2022-12-27 09:04:31 -0300

    Ajustes na impressão das datas em daevento

--- src/NFe/Daevento.php
+++ src/NFe/Daevento.php
@@ -365,12 +365,8 @@ class Daevento extends DaCommon
             'style' => ''
         );
         $this->pdf->textBox($x, $y + 15, $w2, 8, $texto, $aFont, 'T', 'L', 0, '');
-        //$tsHora = $this->toTimestamp($this->dhEvento);
-        //$texto = 'Criado em : ' . date('d/m/Y H:i:s', $tsHora);
         $texto = 'Criado em : ' . \DateTime::createFromFormat("Y-m-d\TH:i:sP", $this->dhEvento)->format('d/m/Y H:i:s');
         $this->pdf->textBox($x, $y + 20, $w2, 8, $texto, $aFont, 'T', 'L', 0, '');
-        //$tsHora = $this->toTimestamp($this->dhRegEvento);
-        //$texto = 'Prococolo: ' . $this->nProt . '  -  Registrado em: ' . date('d/m/Y   H:i:s', $tsHora);
         $texto = 'Prococolo: '
             . $this->nProt
             . ' - Registrado em: '
